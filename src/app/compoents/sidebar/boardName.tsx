f824165dc32f3a212fdd431f237e53c85172dcb6
Anudeep <anudeep.roy@liferay.com>
2023-12-27 14:43:15 +0530

    Added 'Create new Board' link to the sidebar

--- src/app/compoents/sidebar/boardName.tsx
+++ src/app/compoents/sidebar/boardName.tsx
@@ -5,11 +5,15 @@ export default function BoardName ({setBName}:any) {
     return (
         <div className="board-name">
            {data.boards.map((board, index)=>(
-                <a key={index}onClick={()=>setBName(board.name)}>
+                <a key={index}onClick={()=>setBName(board.name, index)}>
                     <img src='/assets/icon-board.svg'/>
                     <h3>{board.name}</h3>
                 </a>
            ))}
+           <a className='new-board'>
+                <img src='/assets/icon-board.svg'/>
+                <h3>+ Create New Board</h3>
+            </a>
         </div>
     )
 }
